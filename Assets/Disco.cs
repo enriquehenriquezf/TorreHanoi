f5c6e54d9498d38c64a4a1903a50647168533d37
enriquehenriquezf <enrique.3427@hotmail.com>
2015-11-17 00:44:17 -0500

    Agregado script Init (inicializar discos) y Script de las propiedades de cada disco

---
+++ Assets/Disco.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disco : MonoBehaviour {
+	public float radio = 2.6f;
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3(radio,transform.localScale.y,radio);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void SetRadio(float r)
+	{
+		radio = r;
+	}
+}
---
+++ Assets/Init.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Init : MonoBehaviour {
+	public int Discos = 3;
+	public GameObject disco;
+	private GameObject disk;
+	public GameObject original;
+	private float radio = 1.4f;
+	// Use this for initialization
+	void Start () {
+		disk = Instantiate(disco, new Vector3(original.GetComponent<Transform>().position.x, original.GetComponent<Transform>().position.y, original.GetComponent<Transform>().position.z), Quaternion.identity) as GameObject;
+		disk.GetComponent<Disco>().SetRadio(this.radio);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
