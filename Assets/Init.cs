34604f81301e08d5c2010d72313535b8264deb87
enriquehenriquezf <enrique.3427@hotmail.com>
2015-11-17 09:38:21 -0500

    Discos creados en orden

--- Assets/Init.cs
+++ Assets/Init.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 
 public class Init : MonoBehaviour {
-	public int Discos = 3;
+	public int Discos = 5;
 	public GameObject disco;
 	private GameObject disk;
 	public GameObject original;
-	private float radio = 1.4f;
+	private float radio = 2.6f;
+	private float pos = 0.2f;
 	// Use this for initialization
 	void Start () {
-		disk = Instantiate(disco, new Vector3(original.GetComponent<Transform>().position.x, original.GetComponent<Transform>().position.y, original.GetComponent<Transform>().position.z), Quaternion.identity) as GameObject;
-		disk.GetComponent<Disco>().SetRadio(this.radio);
+		for (int i = 0; i < Discos; i++) {
+			disk = Instantiate(disco, new Vector3(original.GetComponent<Transform>().position.x, pos, original.GetComponent<Transform>().position.z), Quaternion.identity) as GameObject;
+			disk.GetComponent<Disco>().SetRadio(this.radio);
+			pos = pos + 0.1f;
+			radio = radio - 0.2f;
+		}
 	}
 	
 	// Update is called once per frame
