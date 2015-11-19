ec40fe8f0aeae8f7ac781ffecd667ee7b0b16be8
enriquehenriquezf <enrique.3427@hotmail.com>
2015-11-18 20:00:26 -0500

    Arreglado bugs de iteraciones

--- Assets/Init.cs
+++ Assets/Init.cs
@@ -17,69 +17,72 @@ public class Init : MonoBehaviour {
 	public static Stack<GameObject> Origen = new Stack<GameObject>();
 	public static Stack<GameObject> Aux = new Stack<GameObject>();
 	public static Stack<GameObject> Destino = new Stack<GameObject>();
-<<<<<<< HEAD
 	List<Material> lista;
-=======
-	private float vel = 0f;
-	private float esperar = 0f;
 	private bool wait = true;
 	public static GameObject[] MovG = new GameObject[64];
 	public static float[] MovV1 = new float[64];
 	public static float[] MovV2 = new float[64];
+	public static int[] DirV2 = new int[64];
 	public static float[] MovV3 = new float[64];
 	public int k = 0;
 	private int j = 0;
+	private bool subiendo = false;
 
->>>>>>> Programadas iteraciones, falta perfeccionar animaciones
 	// Use this for initialization
 	void Start () {
 		lista = new List<Material> (colores);
-		for (int i = 0; i < Discos; i++) {
-<<<<<<< HEAD
-			while (lista.Count>0) {
-				disk = Instantiate(disco, new Vector3(original.GetComponent<Transform>().position.x, posO, original.GetComponent<Transform>().position.z), Quaternion.identity) as GameObject;
-				disk.GetComponent<Disco>().SetRadio(this.radio);
-				int x= Random.Range(0,lista.Count-1);
-				disk.GetComponent<Disco>().setMaterial(lista[x]);
-				posO = posO + 0.1f;
-				radio = radio - 0.2f;
-				Origen.Push(disk);
-				lista.RemoveAt(x);
-			}
-=======
+		while (lista.Count>0) {
 			disk = Instantiate(disco, new Vector3(original.transform.position.x, posO, original.transform.position.z), Quaternion.identity) as GameObject;
 			disk.GetComponent<Disco>().SetRadio(this.radio);
+			int x= Random.Range(0,lista.Count-1);
+			disk.GetComponent<Disco>().setMaterial(lista[x]);
 			posO = posO + 0.1f;
-			radio = radio - 0.2f;
+			radio = radio - 0.4f;
 			Origen.Push(disk);
->>>>>>> Programadas iteraciones, falta perfeccionar animaciones
-		}
+			lista.RemoveAt(x);
+		}		
+		posO = posO - 0.1f;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if(j < k && !wait)
 		{
-			if(esperar + 1 > Time.time)// arriba
+			if(MovG[j].transform.position.y < MovV1[j] && subiendo)// arriba
 			{
-				MovG[j].transform.Translate (new Vector3(0,MovV1[j],0));
+				MovG[j].transform.Translate (new Vector3(0,2*Time.deltaTime,0));
 			}
 			else
 			{
-				if(esperar + 2 > Time.time)//izq-der
+				if(MovG[j].transform.position.x < MovV2[j] && DirV2[j] == 1)//der
 				{
-					MovG[j].transform.Translate (new Vector3(MovV2[j],0,0));
+					MovG[j].transform.Translate (new Vector3(2*Time.deltaTime,0,0));
+					if(subiendo)
+					{
+						subiendo = false;
+					}
 				}
 				else
 				{
-					if(esperar + 3 > Time.time)//abajo
+					if(MovG[j].transform.position.x > MovV2[j] && DirV2[j] == -1)//izq
 					{
-						MovG[j].transform.Translate (new Vector3(0,-MovV3[j],0));
+						MovG[j].transform.Translate (new Vector3(-(2*Time.deltaTime),0,0));
+						if(subiendo)
+						{
+							subiendo = false;
+						}
 					}
 					else
 					{
-						esperar = Time.time;
-						j++;
+						if(MovG[j].transform.position.y > MovV3[j])//abajo
+						{
+							MovG[j].transform.Translate (new Vector3(0,-2*Time.deltaTime,0));
+						}
+						else
+						{
+							j++;							
+							subiendo = true;
+						}
 					}
 				}
 			}
@@ -88,8 +91,8 @@ public class Init : MonoBehaviour {
 	public void Hanoi()
 	{
 		ResolverHanoi (Discos,Origen,Destino,Aux,original,final,auxiliar,posO,posD,posA);
+		subiendo = true;
 		wait = false;
-		esperar = Time.time;
 	}
 	private void ResolverHanoi(int n, Stack<GameObject> T1, Stack<GameObject>T2, Stack<GameObject>T3, GameObject O, GameObject F, GameObject A, float Po, float Pf, float Pa)
 	{
@@ -117,24 +120,18 @@ public class Init : MonoBehaviour {
 	private void AnimacionMover(GameObject disc, GameObject o, GameObject f, float po, float pf)
 	{
 		MovG[k] = disc;
-		vel = ((o.transform.position.y+(o.transform.position.y/2))-po)/60;
-		MovV1[k] = vel;
-		//Debug.Log ("arriba: "+ vel);
+		MovV1[k] = 3;// arriba
 		if(o.transform.position.x < f.transform.position.x)
 		{
-			vel = (f.transform.position.x-o.transform.position.x)/60;
-			MovV2[k] = vel;
-			//Debug.Log ("der: "+ (vel));
+			MovV2[k] = f.transform.position.x;// der
+			DirV2[k] = 1;
 		}
 		else
 		{
-			vel = (o.transform.position.x - f.transform.position.x)/60;
-			MovV2[k] = -vel;
-			//Debug.Log ("izq: "+ vel);
+			MovV2[k] = f.transform.position.x;//izq
+			DirV2[k] = -1;
 		}
-		vel = ((o.transform.position.y+(o.transform.position.y/2))-pf)/60;
-		MovV3[k] = vel;		
-		//Debug.Log ("abajo: "+ vel);
+		MovV3[k] = pf;//abajo
 		k++;
 	}
 }
